0fdb58f79c605090daf40726161ca9ff2ebc9b44
appdevs-lee <js100810081@gmail.com>
2024-12-06 17:43:16 +0900

    FEAT: 일일점검 보완

---
+++ RPA-D/Main/Driver/DailyInspectionViewController.swift
@@ -0,0 +1,29 @@
+//
+//  DailyInspectionViewController.swift
+//  RPA-D
+//
+//  Created by Awesomepia on 12/6/24.
+//
+
+import UIKit
+
+class DailyInspectionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
